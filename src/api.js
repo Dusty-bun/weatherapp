7f7055a99edb069d94df57785c9bd98efc4d0d57
Dusty-bun <deniskostow944@gmail.com>
2023-01-06 16:20:49 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "53e780d7f1msh00977f677bc8f76p172b88jsna508c9b9e2e3",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "7c5bf67a16073d161a77036bce562922"; // enter your key from openweather API
